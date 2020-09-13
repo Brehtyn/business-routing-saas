345b8329ae00be596ba396c0c784a2fa2724319f
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-09-13 13:49:49 -0700

    New Changes
    Behold Sir

--- routes/machines.js
+++ routes/machines.js
@@ -4,12 +4,111 @@ const Building = require('../models/building')
 const Machine = require('../models/machine')
 
 
+
+//index to create machines
+router.get('/', async (req, res) => {
+    res.render('machines/createMachIndex')
+})
+
+//index to create new machines
+router.get('/new', async(req, res) =>{
+    res.render('machines/newmachine', {machine: new Machine()})
+})
+
+//creates new machine
+router.post('/new', async(req, res) =>{
+    try{
+        const machine = new Machine({
+            city: req.body.city,
+            location: req.body.location,
+            status: req.body.status,
+            urgent: req.body.urgent,
+            createdAt: new Date(req.body.createdAt)
+        })
+
+        const newMachine = await machine.save()
+        console.log(newMachine)
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+//to delete a machine
+router.get('/remove', (req, res) => {
+    var app = req.app;
+    const allMachinesArray = (app.get('allMachines'))
+
+    try{
+        console.log(allMachinesArray)
+        res.render('machines/deleteMachine', {allMachinesArray: allMachinesArray})
+
+    }catch{
+        res.redirect('/')
+    }
+})
+
+router.delete('/remove/:id', async(req, res) =>{
+    try{
+        const machine = await Machine.findById(req.params.id)
+        await machine.remove()
+        res.redirect('/machines')
+    } catch {
+        if(machine == null){
+            res.redirect('/')
+        }
+        else{
+            console.log('could not remove book')
+            res.redirect('/machines/remove')
+        }
+    }
+})
+
+//to get all machines
+
+router.get('/allmachines', async( req, res) => {
+    var app = req.app
+    const allMachinesArray = (app.get('allMachines'))
+
+    try{
+        res.render('machines/allmachines', {allMachinesArray: allMachinesArray})
+    }catch{
+        res.redirect('/')
+    }
+})
+
+//gets all machines
 router.get('/:id', async (req,res) => {
     try{
         const machine = await Machine.findById(req.params.id)
         res.render('machines/index', {machine: machine})
     }catch{
         res.redirect('/')
+    }
+})
+
+//Edit Building Route
+router.get('/edit/:id', async (req, res) => {
+    try{
+        const machine = await Machine.findById(req.params.id)
+        res.render('machines/edit', {machine: machine})
+    }catch {
+        res.redirect('/machines')
+    }
+})
+
+router.put('/edit/:id',async (req, res) => {
+    try{
+        const machine = await Machine.findById(req.params.id)
+        machine.city = req.body.city,
+        machine.location = req.body.location,
+        machine.status = req.body.status,
+        machine.urgent = req.body.urgent,
+        createdAt = new Date(req.body.createdAt)
+        await machine.save()
+        res.redirect(`/machines/${machine._id}`)
+    } catch{
+        res.redirect('/machines')
     }
 })
 
---
+++ routes/users.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+//Users index route
+router.get('/', async (req, res) =>{
+    res.render('users/index')
+})
+
+//Get all users route
+router.get('/all', async (req, res) =>{
+    let users = []
+    try{
+        users = await User.find().sort({createdAt: 'desc'}).limit(10).exec()
+        console.log(users)
+    } catch(err){
+        console.log(err)
+        users = []
+    }
+    res.render('users/all', {users: users})
+})
+
+//New user route page
+router.get('/new', async(req, res) =>{
+    try{
+    res.render('users/new',  {user: new User()})
+    }catch(err){
+        console.log(err)
+    }
+})
+
+//New user Route
+router.post('/new', async(req, res) =>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        const user = new User({
+            name: req.body.name,
+            password: hashedPassword,
+            authorzationLevel: req.body.authorzationLevel,
+            cell_number: req.body.cell_number,
+            group: req.body.group
+        })
+
+        const newUser = await user.save()
+        res.redirect(`/users/${newUser._id}`)
+    }catch(err){
+        console.log(err)
+        res.redirect('/users')
+    }
+})
+
+//Shows single user page
+router.get('/:id', async (req, res) =>{
+    try{
+        const user = await User.findById(req.params.id)
+        res.render('users/show', {user: user})
+    }catch{
+        res.redirect('/users')
+    }
+})
+
+//Delete User
+router.delete('/delete/:id', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        await user.remove()
+        res.redirect('/users')
+    }catch(err){
+        console.log(err)
+        res.redirect('/users')
+    }
+})
+
+//Edit user Route
+router.get('/edit/:id', async(req, res) =>{
+    try{
+        const user = await User.findById(req.params.id)
+        res.render('users/show', {user: user})
+    }catch{
+        res.redirect('/')
+    }
+})
+
+//Edit user Route
+router.post('/edit/:id', async(req, res) =>{
+    try{
+        const user = await User.findById(req.params.id)
+            user.name =  req.body.name,
+            user.password =  req.body.password,
+            user.authorizationLevel = req.body.authorzationLevel,
+            user.cell_number = req.body.cell_number,
+            user.group = req.body.group
+
+        await user.save()
+        res.redirect(`/users/${user._id}`)
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+module.exports = router
