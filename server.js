884ecfe57c6ef793491d1134398428cdb0f7a0f7
Brehtyn <66739729+Brehtyn@users.noreply.github.com>
2020-09-18 11:18:01 -0700

    login page
    css included
    script for popup
    edits to css referencing
    standardized for any company

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
